92d9d4f38b75a178ae1ea006b7f1762b1465f377
AzukiPL <102173527+AzukiPL@users.noreply.github.com>
2023-03-21 20:30:47 +0100

    records old version

--- main.cpp
+++ main.cpp
@@ -1,17 +1,16 @@
 #include <iostream>
+#include <vector>
 #include "columns.cpp"
 #include "records.cpp"
 int main(int, char**) {
     columns cols;
     records recs;
     cols.CheckDirectories();
-    // cols.columnInt.push_back("test test");
-    for(std::string str : cols.columnInt) {
-        std::cout << str << std::endl;
+    recs.ReadRecords();
+
+    for(auto& elemtn : recs.fileVectors) {
+        for(auto& str : elemtn) {
+            std::cout << str << std::endl;
+        }
     }
-        for(std::string str : cols.columnDecimal) {
-        std::cout << str << std::endl << std::endl << std::endl;
-    }
-    recs.ReadFile("Columns/Int/",cols.columnInt[0]);
-    std::cout << recs.columnRecords[4];
 }
--- records.cpp
+++ records.cpp
@@ -3,13 +3,19 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include "columns.cpp"
 
 class records
 {
-    public:
+    private:
         std::vector<std::string> columnRecords;
-    
+        columns col;
+
     public:
+        std::vector<std::vector<std::string>> fileVectors;
+
+    private:
+    
         void ReadFile(std::string path, std::string file) { // sums path with file name, then opens it and pushes content of that file into vector
             std::fstream columnFile;
             file =  path+file+".txt";
@@ -19,8 +25,42 @@ class records
                 while(std::getline(columnFile,line)) {
                     columnRecords.push_back(line);
                 }
+                fileVectors.push_back(columnRecords);
+                // columnRecords.clear();
                 columnFile.close();
             }
         }
+        void GetRecords(std::string path, std::string file) {
+            ReadFile(path, file);
+            for(auto& line : columnRecords) {
+                 std::cout << line << std::endl;
+            }
+        }
+        void GetVariables(std::string path, std::vector<std::string> directory) {
+            for(auto& str : directory) {
+                GetRecords(path,str);
+            }
+        }
+    public:
+        void ReadRecords() {
+            GetVariables("Columns/Int/", col.columnInt);
+        }
 
+    //         void GetRecords(std::string path) {
+    //             for(auto& vec : fileVectors) {
+    //                 for(auto& str : vec) {
+
+    //                 }
+    //             }
+
+
+    //         ReadFile(path, file);
+    //         for(auto& line : columnRecords) {
+    //              std::cout << line << std::endl;
+    //         }
+    //     }
+    // public:
+    //     void ReadRecords(std::string path, std::string file) {
+    //         GetRecords(path,file);
+    //     }
 };
