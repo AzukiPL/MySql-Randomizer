3621219038bc7aa079d142a2fe57a22b891da9d5
AzukiPL <102173527+AzukiPL@users.noreply.github.com>
2023-03-22 19:51:09 +0100

    fix

--- main.cpp
+++ main.cpp
@@ -28,16 +28,37 @@ void PushVectors() {
 
 int main() {
     PushVectors();
-        int i =0;
+                                // /*         int i =0;
+                                //     for(auto& vec : cols.directory) {
+                                //         if(!vec.empty())
+                                //         for(auto& fileName : vec) {
+                                //             std::cout << fileName << std::endl;
+                                //             recs.ReadFIle(cols.filePath[i],fileName);
+                                //             for(auto& line : recs.contentLine) {
+                                //                 std::cout << line << std::endl;
+                                //             }
+                                //         }
+                                //         i++;
+                                //     } */
+    std::string tableName;
+    std::cout << "enter table name: ";
+    std::cin >> tableName;
+    system("cls");
+    std::cout << "INSERT INTO " << tableName << "(";
+    int i =0;   
     for(auto& vec : cols.directory) {
-        if(!vec.empty())
-        for(auto& fileName : vec) {
-            std::cout << fileName << std::endl;
-            recs.ReadFIle(cols.filePath[i],fileName);
-            for(auto& line : recs.contentLine) {
-                std::cout << line << std::endl;
+        if(!vec.empty()) {
+            if(i==0)
+            std::cout << "\"" << vec[0] << "\"";
+            for(auto& name : vec) {
+                if(i == 0) {
+                    i++;
+                    continue;
+                }
+                std::cout << ",\"" << name << "\"";
             }
         }
-        i++;
     }
+    std::cout << ") VALUES";
+
 }
